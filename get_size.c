1bdbae05dbeab5dce1521fa416e9a9b026d03d58
Simon Kamau <125450074+Pappsyk@users.noreply.github.com>
2023-04-27 17:37:29 +0300

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
@@ -6,6 +7,7 @@
  *
  * Return: Precision.
  */
+
 int get_size(const char *format, int *i)
 {
 int curr_i = *i + 1;
@@ -20,4 +22,3 @@ else
 *i = curr_i;
 return (size);
 }
-
